9d4388c901030cdd648687052553d2fa3e0efdf7
tjuela <tjuela@yahoo.dk>
2016-11-15 12:01:03 +0100

    first run

--- MyModel.R
+++ MyModel.R
@@ -8,9 +8,13 @@ ft = read.csv("features.csv")
 # ft[is.na(ft)]=0
 
 # base linear model -> no train / test set
-m2 <- lm(overalGradeDiff ~ CountofSubmissions + countOfVideoandForumEvents + totalTimeSpent, data=ft)
+m1 <- lm(totalGradeDelta ~ countOfSubmissions + countOfVideoAndForumEvents + totalTimeSpent, data=ft)
 
 set.seed(1234)
 tr.index= sample(nrow(ft), nrow(ft)*0.6)
 ft.train= ft[tr.index,]
 ft.test = ft[-tr.index,]
+
+m1_train = train(totalGradeDelta ~ countOfSubmissions + countOfVideoAndForumEvents + totalTimeSpent, data=ft.train, method="lm")
+
+summary(m1_train)$r.squared
--- aggregateFeatures.R
+++ aggregateFeatures.R
@@ -11,12 +11,14 @@ View(db)
   agg.features = ddply(db,
                        .(UserID,ProblemID),
                        summarise,
-                       overalGradeDiff = Grade[length(Grade)]-Grade[1],
-                       CountofSubmissions = length(SubmissionNumber),
-                       countOfVideoandForumEvents = (sum(NVideoEvents, na.rm = T) + sum(NForumEvents,na.rm = T)),
-                       totalTimeSpent = TimeStamp[length(TimeStamp)] - TimeStamp[1])
+                       totalGradeDelta = Grade[length(Grade)]-Grade[1],
+                       bestGrade = max(Grade),
+                       countOfSubmissions = length(SubmissionNumber),
+                       countOfVideoAndForumEvents = (sum(NVideoEvents, na.rm = T) + sum(NForumEvents,na.rm = T)),
+                       totalTimeSpent = TimeStamp[length(TimeStamp)] - TimeStamp[1],
+                       avgTimeSpentPrSubmission = totalTimeSpent/countOfSubmissions)
 #------ remove cases with only one attempt
-  agg.features=filter(agg.features,CountofSubmissions>1); dim(agg.features)
+  #agg.features=filter(agg.features,CountofSubmissions>1); dim(agg.features)
 #------ save feature file
   write.csv(agg.features, file='features.csv')
  
